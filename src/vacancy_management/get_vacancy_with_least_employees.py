890d80baf08a3f2f3b75227b81d623c4ac9e8efa
shadowplay1 <shadowplay483@gmail.com>
2024-03-30 12:54:03 +0300

    fix(get_vacancy_with_least_employees): getting incorrect vacancy

--- src/vacancy_management/get_vacancy_with_least_employees.py
+++ src/vacancy_management/get_vacancy_with_least_employees.py
@@ -2,6 +2,9 @@ from ..definitions import Vacancy
 from ...src import VacancyManager
 
 vacancy_manager = VacancyManager('data/vacancy.csv')
-vacancy_with_least_employees: Vacancy = list(filter(lambda vacancy: vacancy[3].lower() == 'классный руководитель', vacancy_manager.get_vacancies_with_least_employees()))[0]
 
+sorted_vacancies = vacancy_manager.get_vacancies_with_least_employees()
+required_vacancy_name = 'классный руководитель'
+
+vacancy_with_least_employees: Vacancy = list(filter(lambda vacancy: vacancy[3].lower() == required_vacancy_name, sorted_vacancies))[0]
 print(f'В компании {vacancy_with_least_employees[4]} есть заданная профессия: {vacancy_with_least_employees[3]}, з/п в такой компании составит: {vacancy_with_least_employees[0]}')
